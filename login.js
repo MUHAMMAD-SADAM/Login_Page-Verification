557c1821009b9004209696afc9b1984106c7a2f3
SadamMuneer <sadammuneer390@gmail.com>
2024-02-21 00:29:25 +0500

    Login_Page

---
+++ login.js
@@ -0,0 +1,30 @@
+//Email verification
+function validateEmail(email) {
+  const emailError = document.getElementById("emailError");
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if (email.trim() === "") {
+    emailError.textContent = "Email is required.";
+  } else if (!emailRegex.test(email)) {
+    emailError.textContent = "Invalid email address.";
+  } else {
+    emailError.textContent = "";
+  }
+}
+
+// password verification
+function togglePassword() {
+  const pass = document.getElementById("pass");
+  const check = document.getElementById("check");
+
+  if (pass.type === "password") {
+    pass.type = "text";
+    check.classList.remove("fa-eye");
+    check.classList.add("fa-eye-slash");
+  } else {
+    pass.type = "password";
+    check.classList.remove("fa-eye-slash");
+    check.classList.add("fa-eye");
+  }
+}
+//
